852a29496b457d8ad7a48c176b7e9b70fd093330
kadu <kaduzera.ks@gmail.com>
2022-01-29 02:57:30 -0300

    .

--- src/players/pipes/players-validation-parameters.pipe.ts
+++ src/players/pipes/players-validation-parameters.pipe.ts
@@ -4,6 +4,7 @@ import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/co
 export class PlayersValidationParameters implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
     if(!value) throw new BadRequestException(`O valor do parametro ${metadata.data} deve ser informado`)
+    
     return value
   }
 }
